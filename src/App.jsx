2a1358b3fd45f0ae5744726e97503c77dd3d7171
GaYoung Kim <kimkayeong182@naver.com>
2026-04-08 21:43:08 +0900

    feat: 시스템 로그인 완료
    - 아이디 또는 비밀번호가 일치하지 않을 때에 대한 처리 수정 가능성 있음

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/home/Home";
 import Login from "./pages/auth/Login/Login";
 import SystemLogin from "./pages/auth/Login/SystemLogin";
 import Signup from "./pages/auth/Signup/Signup";
+import Find from "./pages/auth/Find/Find";
 
 import Community from "./pages/community/Community";
 
@@ -15,13 +16,19 @@ import MapPage from "./pages/map/MapPage";
 function App() {
   return (
     <Routes>
+      {/* 로그인 관련 */}
       <Route path="/" element={<Login />} />
       <Route path="/oauth/*" element={<Login />} />{" "}
       <Route path="/login" element={<SystemLogin />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="/auth/find" element={<Find />} />
+      {/* 홈 관련 */}
       <Route path="/home" element={<Home />} />
+      {/* 커뮤니티 관련 */}
       <Route path="/community" element={<Community />} />
+      {/* 마이페이지 관련 */}
       <Route path="/mypage" element={<MyPage />} />
+      {/* 지도 관련 */}
       <Route path="/map" element={<MapPage />} />
     </Routes>
   );
---
+++ src/pages/auth/Find/Find.jsx
@@ -0,0 +1,7 @@
+export default function Find() {
+  return (
+    <div>
+      <h1>아이디/비밀번호 찾기</h1>
+    </div>
+  );
+}
