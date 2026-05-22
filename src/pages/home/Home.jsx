75f0895b9411a6f7431d3fa370b1299ed6bfa2a6
JuHyeong <blane7777@naver.com>
2026-05-22 23:22:20 +0900

    feat: 홈화면, 산책 결과조회, 주간 활동 리포트, 카메라 접근 및 촬영, AI 스마트 진단 리포트 UI 구현

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import "./Home.css";
 import Terms from "../auth/Signup/Terms";
